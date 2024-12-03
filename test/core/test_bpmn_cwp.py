9698e7eee868e778e7cfe329c1cb2ac3cce8f2b7
Ty Klabacka <klabacka.ty@gmail.com>
2024-12-03 22:25:50 +0000

    Add test for no end events

--- test/core/test_bpmn_cwp.py
+++ test/core/test_bpmn_cwp.py
@@ -6,7 +6,7 @@ from bpmncwpverify.core.state import SymbolTable
 from returns.result import Failure
 
 
-def test_complete_bpmn_with_no_start_state_process():
+def test_complete_bpmn_with_no_start_or_end_event():
     symbol_table = SymbolTable()
 
     root = Element(
@@ -41,3 +41,48 @@ def test_complete_bpmn_with_no_start_state_process():
     assert isinstance(result, Failure)
     exception = result.failure()
     assert exception == "Error with end events or start events: # end events = 0, # start events = 0"
+
+
+def test_complete_bpmn_with_no_end_event():
+    symbol_table = SymbolTable()
+
+    root = Element(
+        "bpmn:definitions",
+        attrib={
+            "xmlns:bpmn": NAMESPACES["bpmn"],
+            "id": "Definitions_1",
+            "targetNamespace": "http://example.com/schema/bpmn",
+        },
+    )
+
+    collab = SubElement(root, "bpmn:collaboration", attrib={"id": "Collaboration_1"})
+    SubElement(
+        collab,
+        "bpmn:participant",
+        attrib={
+            "id": "Participant_1",
+            "name": "Test Participant",
+            "processRef": "Process_1",
+        },
+    )
+
+    process = SubElement(
+        root, "bpmn:process", attrib={"id": "Process_1", "isExecutable": "false"}
+    )
+    start = SubElement(process, "bpmn:startEvent", attrib={"id": "se1"})
+    outgoing = SubElement(start, "bpmn:outgoing")
+    outgoing.text = "flow1"
+    SubElement(process, "bpmn:exclusiveGateway", attrib={"id": "eg1"})
+    SubElement(
+        process,
+        "bpmn:sequenceFlow",
+        attrib={"id": "flow1", "sourceRef": "se1", "targetRef": "eg1"},
+    )
+
+    bpmn = tostring(root, encoding="unicode")
+
+    root = ElementTree.fromstring(bpmn)
+    result = Bpmn.from_xml(root, symbol_table)
+    assert isinstance(result, Failure)
+    exception = result.failure()
+    assert exception == "Error with end events or start events: # end events = 0, # start events = 1"
