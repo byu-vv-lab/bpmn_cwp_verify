98ea7958751c490c40b80edf638cb3043093fbd3
Ty Klabacka <klabacka.ty@gmail.com>
2025-02-06 02:09:20 +0000

    Fix file paths in tests

--- test/test_cli.py
+++ test/test_cli.py
@@ -131,22 +131,22 @@ def test_givin_good_files_when_verify_then_output_promela(capsys):
 def test_good_input_webverify_output_promela():
     # given
     bpmn = ""
-    with open("./test/resources/test_bpmn.bpmn", "r") as bpmn_file:
+    with open("./test/resources/simple_example/test_bpmn.bpmn", "r") as bpmn_file:
         for line in bpmn_file:
             bpmn += line
 
     cwp = ""
-    with open("./test/resources/test_cwp.xml", "r") as cwp_file:
+    with open("./test/resources/simple_example/test_cwp.xml", "r") as cwp_file:
         for line in cwp_file:
             cwp += line
 
     state = ""
-    with open("./test/resources/state.txt", "r") as state_file:
+    with open("./test/resources/simple_example/state.txt", "r") as state_file:
         for line in state_file:
             state += line
 
     behavior = ""
-    with open("./test/resources/behavior.txt", "r") as behavior_file:
+    with open("./test/resources/simple_example/behavior.txt", "r") as behavior_file:
         for line in behavior_file:
             behavior += line
     # when
@@ -162,22 +162,22 @@ def test_good_input_webverify_output_promela():
 def test_bad_input_webverify_output_error():
     # given
     bpmn = ""
-    with open("./test/resources/test_bpmn.bpmn", "r") as bpmn_file:
+    with open("./test/resources/simple_example/test_bpmn.bpmn", "r") as bpmn_file:
         for line in bpmn_file:
             bpmn += line
 
     cwp = ""
-    with open("./test/resources/test_cwp.xml", "r") as cwp_file:
+    with open("./test/resources/simple_example/test_cwp.xml", "r") as cwp_file:
         for line in cwp_file:
             cwp += line
 
     state = ""
-    with open("./test/resources/bad_state.txt", "r") as state_file:
+    with open("./test/resources/simple_example/bad_state.txt", "r") as state_file:
         for line in state_file:
             state += line
 
     behavior = ""
-    with open("./test/resources/behavior.txt", "r") as behavior_file:
+    with open("./test/resources/simple_example/behavior.txt", "r") as behavior_file:
         for line in behavior_file:
             behavior += line
     # when
