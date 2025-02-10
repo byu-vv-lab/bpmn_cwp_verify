367e06dd831772af4dec60f1457836892a60088f
Ty Klabacka <klabacka.ty@gmail.com>
2025-02-10 23:43:58 +0000

    remove unnecessary code

--- src/bpmncwpverify/core/spin.py
+++ src/bpmncwpverify/core/spin.py
@@ -1,13 +1,6 @@
 from typing import List
 
-from antlr4 import CommonTokenStream, InputStream
-from antlr4.error.ErrorStrategy import ParseCancellationException
-from bpmncwpverify.antlr.SpinLexer import SpinLexer
-from bpmncwpverify.antlr.StateParser import StateParser
-from bpmncwpverify.core.error import Error, StateSyntaxError
 from returns.maybe import Maybe
-from returns.result import Failure, Result, Success
-from bpmncwpverify.antlr.SpinListener import SpinListener
 import subprocess
 
 
@@ -25,31 +18,6 @@ class SpinOutput:
 
 def get_spin_output(file_path: str) -> str:
     result = subprocess.run(
-        ["spin", "-run", file_path], capture_output=True, text=True
+        ["spin", "-run", "-noclaim", file_path], capture_output=True, text=True
     ).stdout
     return result
-
-
-class Listener(SpinListener):  # type: ignore
-    pass
-
-
-def _get_parser(file_contents: str) -> Result[StateParser, Error]:
-    input_stream = InputStream(file_contents)
-    lexer = SpinLexer(input_stream)
-    stream = CommonTokenStream(lexer)
-    parser = StateParser(stream)
-    return Success(parser)
-
-
-def _parse_state(parser: StateParser) -> Result[StateParser.StateContext, Error]:
-    try:
-        tree: StateParser.StateContext = parser.state()
-        return Success(tree)
-    except ParseCancellationException as exception:
-        msg = str(exception)
-        failure_value = StateSyntaxError(msg)
-        return Failure(failure_value)
-
-
-# _parse_state(_get_parser(get_spin_output(os.getcwd() + 'test/resources/simple_example/valid_output.pml')))
