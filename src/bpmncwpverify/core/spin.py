9fbcd4c0caddfe122f28e9bd4afdeec1c9c3473b
Ty Klabacka <klabacka.ty@gmail.com>
2025-02-11 20:52:58 +0000

    Add flow

--- src/bpmncwpverify/core/spin.py
+++ src/bpmncwpverify/core/spin.py
@@ -1,33 +1,31 @@
 from typing import List
 
+from returns.result import Failure, Result, Success
 from bpmncwpverify.core.error import Error
 from returns.maybe import Maybe, Nothing, Some
+from returns.pipeline import flow
 import subprocess
 import re
 
 
-class SpinSummary:
-    pass
-
-
 class SpinOutput:
-    __slots__ = ["error_num", "error_trace", "coverage"]
+    __slots__ = ["spin_msg", "error_num", "error_trace", "coverage"]
 
-    def __init__(self) -> None:
+    def __init__(self, spin_msg: str) -> None:
+        self.spin_msg = spin_msg
         self.error_num: Maybe[int] = Nothing
         self.error_trace: Maybe[List[Error]] = Nothing
-        self.coverage: SpinSummary = SpinSummary()
+        self.coverage: str = ""
 
-    def _get_errors(self, s: str) -> "SpinOutput":
-        error_num = re.search(r"errors: ([0-9]+)", s)
+    def _get_errors(self) -> "SpinOutput":
+        error_num = re.search(r"errors: ([0-9]+)", self.spin_msg)
         if error_num:
             self.error_num = Some(int(error_num.group(1)))
         else:
             assert False, "There should always be an error trace"
-
         return self
 
-    def _check_syntax_errors(self, s: str) -> "SpinOutput":
+    def _check_syntax_errors(self) -> "SpinOutput":
         charref = re.compile(
             r"""
             (?:[\w/.:\s]*)        # Discard the 'spin: file path'
@@ -36,15 +34,25 @@ class SpinOutput:
         """,
             re.VERBOSE,
         )
-        iterator = charref.finditer(s)
-        self.error_trace = Some([error.groups() for error in iterator])
+        iterator = charref.finditer(self.spin_msg)
+        stx_errors = [error.groups() for error in iterator]
+        self.error_trace = Some(stx_errors)
+        self.error_num = Some(len(stx_errors))
         return self
 
     @staticmethod
-    def get_spin_output(file_path: str) -> "SpinOutput":
-        spin_output = SpinOutput()
-        result = subprocess.run(
-            ["spin", "-run", "-noclaim", file_path], capture_output=True, text=True
-        ).stdout
+    def get_spin_output(file_path: str) -> Result["SpinOutput", Error]:
+        try:
+            result = subprocess.run(
+                ["spin", "-run", "-noclaim", file_path], capture_output=True, text=True
+            ).stdout
 
-        return spin_output._get_errors(result)
+            spin_output = flow(
+                SpinOutput(result),
+                SpinOutput._get_errors,
+                SpinOutput._check_syntax_errors,
+            )
+            __import__("pdb").set_trace()
+            return Success(spin_output)
+        except Exception as e:
+            return Failure(e)
--- test/core/test_spin.py
+++ test/core/test_spin.py
@@ -1,6 +1,6 @@
 from bpmncwpverify.core.spin import SpinOutput
 import pytest
-from returns.maybe import Some
+from returns.maybe import Some, Maybe
 
 
 def test_get_errors_0_errors(mocker):
@@ -12,8 +12,9 @@ def test_get_errors_0_errors(mocker):
             17750 states, stored
         Words...
     """
+    mock_spin_output.spin_msg = s
 
-    SpinOutput._get_errors(mock_spin_output, s)
+    SpinOutput._get_errors(mock_spin_output)
     assert mock_spin_output.error_num == Some(0)
 
 
@@ -26,16 +27,18 @@ def test_get_errors_15_errors(mocker):
             17750 states, stored
         Words...
     """
+    mock_spin_output.spin_msg = s
 
-    SpinOutput._get_errors(mock_spin_output, s)
+    SpinOutput._get_errors(mock_spin_output)
     assert mock_spin_output.error_num == Some(15)
 
 
 def test_get_errors_bad_input(mocker):
     mock_spin_output = mocker.Mock()
+    mock_spin_output.spin_msg = ""
 
     with pytest.raises(AssertionError) as exc_info:
-        SpinOutput._get_errors(mock_spin_output, "")
+        SpinOutput._get_errors(mock_spin_output)
 
     assert exc_info.value.args[0] == "There should always be an error trace"
 
@@ -46,10 +49,17 @@ def test_check_syntax_errors(mocker):
     spin: test/resources/simple_example/valid_output.pml:55, Error: syntax error    saw ''}' = 125'
     spin: test/resources/simple_example/valid_output.pml:116, Error: missing '}' ?
     """
-    SpinOutput._check_syntax_errors(mock_spin_output, s)
-    assert mock_spin_output.error_trace[0][0] == "55"
-    assert (
-        mock_spin_output.error_trace[0][1] == "Error: syntax error    saw ''}' = 125'"
-    )
-    assert mock_spin_output.error_trace[1][0] == "116"
-    assert mock_spin_output.error_trace[1][1] == "Error: missing '}' ?"
+    mock_spin_output.spin_msg = s
+    SpinOutput._check_syntax_errors(mock_spin_output)
+    match mock_spin_output.error_trace:
+        case Some(x):
+            assert x[0][0] == "55"
+            assert x[0][1] == "Error: syntax error    saw ''}' = 125'"
+            assert x[1][0] == "116"
+            assert x[1][1] == "Error: missing '}' ?"
+        case Maybe.empty:
+            assert False
+
+
+def test_get_spin_output(mocker):
+    pass
