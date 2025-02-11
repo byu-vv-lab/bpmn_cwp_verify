a25252f3e382ac1a2b317654ddf9ce97e011ac72
Ty Klabacka <klabacka.ty@gmail.com>
2025-02-11 17:41:47 +0000

    Add method for getting errors

--- src/bpmncwpverify/core/spin.py
+++ src/bpmncwpverify/core/spin.py
@@ -1,7 +1,9 @@
 from typing import List
 
-from returns.maybe import Maybe
+from bpmncwpverify.core.error import Error
+from returns.maybe import Maybe, Nothing, Some
 import subprocess
+import re
 
 
 class SpinSummary:
@@ -9,15 +11,28 @@ class SpinSummary:
 
 
 class SpinOutput:
-    __slots__ = ["error", "coverage"]
+    __slots__ = ["error_num", "error_trace", "coverage"]
 
     def __init__(self) -> None:
-        self.error: Maybe[List[str]]
-        self.coverage: SpinSummary
+        self.error_num: Maybe[int] = Nothing
+        self.error_trace: Maybe[List[Error]] = Nothing
+        self.coverage: SpinSummary = SpinSummary()
 
+    def _get_errors(self, s: str) -> "SpinOutput":
+        error_num = re.search(r"errors: ([0-9])+", s)
+        if error_num:
+            self.error_num = Some(int(error_num.group(1)))
+        else:
+            assert False, "There should always be an error trace"
 
-def get_spin_output(file_path: str) -> str:
-    result = subprocess.run(
-        ["spin", "-run", "-noclaim", file_path], capture_output=True, text=True
-    ).stdout
-    return result
+        return self
+
+    @staticmethod
+    def get_spin_output(file_path: str) -> "SpinOutput":
+        spin_output = SpinOutput()
+        result = subprocess.run(
+            ["spin", "-run", "-noclaim", file_path], capture_output=True, text=True
+        ).stdout
+        # todo: first add check to see if there were any syntax errors in the promela
+
+        return spin_output._get_errors(result)
