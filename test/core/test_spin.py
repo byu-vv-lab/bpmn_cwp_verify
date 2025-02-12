7d1e57cfb9f6adc24094dd5f8e9e9e72d89ca96e
Ty Klabacka <klabacka.ty@gmail.com>
2025-02-12 17:56:03 +0000

    Add test for bad result

--- test/core/test_spin.py
+++ test/core/test_spin.py
@@ -1,8 +1,9 @@
 from bpmncwpverify.core.spin import SpinOutput
-from returns.result import Success
+from returns.result import Success, Failure
 import pytest
 from returns.maybe import Some, Maybe, Nothing
 from returns.pipeline import is_successful
+from returns.functions import not_
 
 
 def test_get_errors_0_errors(mocker):
@@ -81,3 +82,22 @@ def test_get_spin_output(mocker):
     result = SpinOutput.get_spin_output("")
     assert isinstance(result, Success)
     assert is_successful(result)
+
+
+def test_get_spin_output_bad_return_failure(mocker):
+    mock_return_val = mocker.Mock()
+    mocker.patch("bpmncwpverify.core.spin.subprocess.run", return_value=mock_return_val)
+    mock_return_val.stdout = "test_string"
+    mocker.patch("bpmncwpverify.core.spin.SpinOutput")
+    mocker.patch(
+        "bpmncwpverify.core.spin.SpinOutput._get_errors",
+        return_value=Failure(mocker.Mock()),
+    )
+    mocker.patch(
+        "bpmncwpverify.core.spin.SpinOutput._check_syntax_errors",
+        return_value=Success(mocker.Mock()),
+    )
+
+    result = SpinOutput.get_spin_output("")
+    assert isinstance(result, Failure)
+    assert not_(is_successful)(result)
