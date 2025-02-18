f22e61dc485dcc2bae1073eb119978a337cd84da
Ty Klabacka <klabacka.ty@gmail.com>
2025-02-18 02:38:39 +0000

    Fix check syntax errors

--- src/bpmncwpverify/core/spin.py
+++ src/bpmncwpverify/core/spin.py
@@ -1,6 +1,7 @@
-from returns.result import Result, Success
-from bpmncwpverify.core.error import Error
+from returns.result import Result, Success, Failure
+from bpmncwpverify.core.error import Error, SpinSyntaxError
 import subprocess
+import re
 
 
 class Coverage:
@@ -8,11 +9,23 @@ class Coverage:
 
 
 class SpinOutput:
-    def _get_errors(self) -> Result[Coverage, Error]:
+    def _get_errors(self, spin_msg: str) -> Result[Coverage, Error]:
         return Success(Coverage())
 
-    def _check_syntax_errors(self) -> Result[Coverage, Error]:
-        return Success(Coverage())
+    def _check_syntax_errors(self, spin_msg: str) -> Result[Coverage, Error]:
+        r = re.compile(
+            r"""
+            spin: (?P<file_path>.*?):(?P<line_number>\d+),\sError: (?P<error_msg>.*)
+            """,
+            re.VERBOSE,
+        )
+
+        errors = [
+            {k: re.sub(r"\s+", " ", v).strip() for k, v in t.groupdict().items()}
+            for t in r.finditer(spin_msg)
+        ]
+
+        return Failure(SpinSyntaxError(errors)) if errors else Success(Coverage())
 
     @staticmethod
     def get_spin_output(file_path: str) -> Result["SpinOutput", Error]:
