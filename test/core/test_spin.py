4a2d887a26dbb563e8fabec7dbb1c8df7af00588
Ty Klabacka <klabacka.ty@gmail.com>
2025-02-18 02:54:27 +0000

    Add test for check_syntax

--- test/core/test_spin.py
+++ test/core/test_spin.py
@@ -1,50 +1,7 @@
 from bpmncwpverify.core.spin import SpinOutput
 from returns.result import Success, Failure
-import pytest
-from returns.maybe import Some, Maybe, Nothing
 from returns.pipeline import is_successful
 from returns.functions import not_
-
-
-def test_get_errors_0_errors(mocker):
-    mock_spin_output = mocker.Mock()
-
-    s = """
-        Words...
-        State-vector 28 byte, depth reached 9999, errors: 0
-            17750 states, stored
-        Words...
-    """
-    mock_spin_output.spin_msg = s
-    mock_spin_output.error_num = Nothing
-
-    SpinOutput._get_errors(mock_spin_output)
-    assert mock_spin_output.error_num == Nothing
-
-
-def test_get_errors_15_errors(mocker):
-    mock_spin_output = mocker.Mock()
-
-    s = """
-        Words...
-        State-vector 28 byte, depth reached 9999, errors: 15
-            17750 states, stored
-        Words...
-    """
-    mock_spin_output.spin_msg = s
-
-    SpinOutput._get_errors(mock_spin_output)
-    assert mock_spin_output.error_num == Some(15)
-
-
-def test_get_errors_bad_input(mocker):
-    mock_spin_output = mocker.Mock()
-    mock_spin_output.spin_msg = ""
-
-    with pytest.raises(AssertionError) as exc_info:
-        SpinOutput._get_errors(mock_spin_output)
-
-    assert exc_info.value.args[0] == "There should always be an error trace"
 
 
 def test_check_syntax_errors(mocker):
@@ -53,16 +10,25 @@ def test_check_syntax_errors(mocker):
     spin: test/resources/simple_example/valid_output.pml:55, Error: syntax error    saw ''}' = 125'
     spin: test/resources/simple_example/valid_output.pml:116, Error: missing '}' ?
     """
-    mock_spin_output.spin_msg = s
-    SpinOutput._check_syntax_errors(mock_spin_output)
-    match mock_spin_output.error_trace:
-        case Some(x):
-            assert x[0][0] == "55"
-            assert x[0][1] == "Error: syntax error    saw ''}' = 125'"
-            assert x[1][0] == "116"
-            assert x[1][1] == "Error: missing '}' ?"
-        case Maybe.empty:
-            assert False
+
+    result = SpinOutput._check_syntax_errors(mock_spin_output, s)
+
+    assert isinstance(result, Failure)
+    result = result.failure()
+    assert (
+        result.list_of_error_maps[0]["file_path"]
+        == "test/resources/simple_example/valid_output.pml"
+    )
+    assert (
+        result.list_of_error_maps[1]["file_path"]
+        == "test/resources/simple_example/valid_output.pml"
+    )
+
+    assert result.list_of_error_maps[0]["line_number"] == "55"
+    assert result.list_of_error_maps[1]["line_number"] == "116"
+
+    assert result.list_of_error_maps[0]["error_msg"] == "syntax error saw ''}' = 125'"
+    assert result.list_of_error_maps[1]["error_msg"] == "missing '}' ?"
 
 
 def test_get_spin_output(mocker):
