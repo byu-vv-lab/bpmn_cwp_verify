41a0a3bbf7dc5216c7ab89cbf1c82e5d749e8515
Ty Klabacka <klabacka.ty@gmail.com>
2025-03-14 01:42:31 +0000

    Add visit/end-visit methods to CwpPromelaVisitor

--- src/bpmncwpverify/visitors/cwppromelavisitor.py
+++ src/bpmncwpverify/visitors/cwppromelavisitor.py
@@ -1,5 +1,21 @@
-from bpmncwpverify.core.cwp import CwpVisitor
+from bpmncwpverify.core.cwp import Cwp, CwpEdge, CwpState, CwpVisitor
 
 
 class CwpPromelaVisitor(CwpVisitor):  # type: ignore
-    pass
+    def visit_state(self, state: CwpState) -> bool:
+        return True
+
+    def end_visit_state(self, state: CwpState) -> None:
+        pass
+
+    def visit_edge(self, edge: CwpEdge) -> bool:
+        return True
+
+    def end_visit_edge(self, edge: CwpEdge) -> None:
+        pass
+
+    def visit_cwp(self, model: Cwp) -> bool:
+        return True
+
+    def end_visit_cwp(self, model: Cwp) -> None:
+        pass
