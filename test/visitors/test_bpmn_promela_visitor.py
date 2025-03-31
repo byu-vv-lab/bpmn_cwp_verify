70b9e91c56e8ca9c6311497f7921b342489f486e
jpmlf <fitzosapian@gmail.com>
2025-03-31 22:14:24 +0000

    fix bpmn test to include refrence to cwp

--- test/visitors/test_bpmn_promela_visitor.py
+++ test/visitors/test_bpmn_promela_visitor.py
@@ -348,7 +348,8 @@ def test_gen_behavior_model(promela_visitor, mocker):
 
     promela_visitor._gen_behavior_model(ctx)
     assert (
-        str(promela_visitor.behaviors) == "inline TEST_BehaviorModel(){\n\tskip\n}\n\n"
+        str(promela_visitor.behaviors)
+        == "inline TEST_BehaviorModel(){\n\tskip\n\tUpdate_State()\n}\n\n"
     )
 
 
