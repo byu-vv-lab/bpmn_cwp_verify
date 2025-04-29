f0332cc367ea83cfff2d0ae3a6886f246de8d118
jpmlf <fitzosapian@gmail.com>
2025-04-29 18:16:23 +0000

    Squashed and merged add-cwp-mapping-function into mapping-function-rebase

--- src/bpmncwpverify/visitors/cwppromelavisitor.py
+++ src/bpmncwpverify/visitors/cwppromelavisitor.py
@@ -12,14 +12,32 @@ END_STR = "//**********************************************//"
 
 class CwpPromelaVisitor(CwpVisitor):  # type: ignore
     def __init__(self) -> None:
-        # self.start = StringManager()
         self.cwp_states = StringManager()
         self.update_state_inline = StringManager()
-        # self.end = StringManager()
+        self.mapping_function = StringManager()
 
     def visit_state(self, state: CwpState) -> bool:
         new_str = f"bool {state.name} = false"
         self.cwp_states.write_str(new_str, NL_SINGLE)
+
+        # input edges
+        self.mapping_function.write_str("::((")
+        for edge in state.in_edges:
+            self.mapping_function.write_str(f"{edge.expression} && ")
+
+        # ensure the ending in && doesnt break the promela
+        self.mapping_function.write_str("true ")
+
+        self.mapping_function.write_str(") && !(")
+
+        for edge in state.out_edges:
+            self.mapping_function.write_str(f"{edge.expression} ||")
+
+        # ensure the ending in || doesnt break the promela
+        self.mapping_function.write_str("false ")
+
+        self.mapping_function.write_str(f")) -> {state.name} = true", NL_SINGLE)
+
         return True
 
     def end_visit_state(self, state: CwpState) -> None:
@@ -31,11 +49,21 @@ class CwpPromelaVisitor(CwpVisitor):  # type: ignore
     def create_update_state_inline(
         self,
     ) -> None:
-        new_str = "inline updateState() {"
+        new_str = "inline Update_State() {"
         self.update_state_inline.write_str(new_str, NL_SINGLE, IndentAction.INC)
-        self.update_state_inline.write_str("skip", NL_SINGLE)
 
         # inside of update state inline will go here
+
+        # start of the if statement
+        self.update_state_inline.write_str("if", NL_SINGLE, IndentAction.INC)
+
+        # update state inline sm appends mapping function sm
+        self.update_state_inline.write_str(self.mapping_function, NL_SINGLE)
+
+        self.update_state_inline.write_str(":: else -> assert false", NL_SINGLE)
+
+        # end of if statement
+        self.update_state_inline.write_str("fi", NL_SINGLE, IndentAction.DEC)
 
         self.update_state_inline.write_str("}", NL_SINGLE, IndentAction.DEC)
         pass
