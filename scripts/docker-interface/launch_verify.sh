9670fbb17208e42c83eaea23a4234d4142ffe6e6
Eric Mercer <egm@cs.byu.edu>
2025-06-04 21:45:37 +0000

    Remove code and scripts from original code base
    Close #272

--- scripts/docker-interface/launch_verify.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-(cd code/src ; python3 main.py $@)
--- scripts/example-runners/run_premade_examples.sh
+++
@@ -1,4 +0,0 @@
-python3 main.py -e buynsell_Sep_16_2023 -I
-python3 main.py -e face2face_Sep_16_2023 -I
-python3 main.py -e face2face_Sep_16_2023_error -I
-python3 main.py -e phware_Sep_16_2023 -I
